46d2c5c3797ecdf009be7c70401aa5612b5af60e
LahariKemburu <146975432+LahariKemburu@users.noreply.github.com>
2024-01-29 15:45:15 +0530

    Update simonjs.js

--- simonjs.js
+++ simonjs.js
@@ -1,110 +1,121 @@
 let gameSequence = [];
-let userSequence = [];
-let gameStarted = false;
-let level = 0;
+        let userSequence = [];
+        let gameStarted = false;
+        let level = 0;
 
-const colors = ["red", "yellow", "green", "violet"];
-const h2 = document.querySelector("h2");
-const h1=document.querySelector("h1");
+        const colors = ["red", "yellow", "green", "purple"];
+        const h2 = document.querySelector("h2");
+        const h1 = document.querySelector("h1");
+        const buttons = document.querySelectorAll(".button");
 
-function buttonFlash(button) {
-    button.classList.add("flash");
-    setTimeout(function () {
-        button.classList.remove("flash");
-    }, 250);
-}
-
-document.addEventListener("keypress", function () {
-    if (!gameStarted) {
-        startGame();
-    }
-});
-
-
-document.querySelector(".start").addEventListener("click", function () {
-    if (!gameStarted) {
-        startGame();
-        updatebutton("restart");
-    }
-    else{
-        updatebutton("Start");
-        location.reload();
-    }
-    
-});
-
-function updatebutton(text)
-{
-    const sb=document.querySelector(".start");
-    sb.innerText=text;
-    sb.classList.toggle("restart");
-
-}
-
-function startGame() {
-    gameStarted = true;
-    gameSequence = [];
-    userSequence = [];
-    level = 0;
-    nextLevel();
-}
-
-function nextLevel() {
-    level++;
-    h2.innerText = `Level ${level}`;
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    gameSequence.push(randomColor);
-
-    playSequence();
-}
-
-function playSequence() {
-    let i = 0;
-    const interval = setInterval(function () {
-        buttonFlash(document.querySelector(`.${gameSequence[i]}`));
-        i++;
-        if (i >= gameSequence.length) {
-            clearInterval(interval);
+        async function buttonFlash(button) {
+            await Flash(button);
         }
-    }, 1000);
 
-    userSequence = [];
-}
+        function Flash(button) {
+            return new Promise((res) => {
+                button.classList.add("flash");
+                setTimeout(() => {
+                    button.classList.remove("flash");
+                    res();
+                }, 250);
+            });
+        }
 
-function checkUserInput() {
-    if (userSequence.length === gameSequence.length) {
-        for (let i = 0; i < userSequence.length; i++) {
-            if (userSequence[i] !== gameSequence[i]) {
-                gameOver();
-                return;
+        document.addEventListener("keypress", function () {
+            if (!gameStarted) {
+                startGame();
+            }
+        });
+
+        document.querySelector(".start").addEventListener("click", function () {
+            if (!gameStarted) {
+                startGame();
+                updateButton("restart");
+            } else {
+                // updateButton("Start");
+                location.reload();
+            }
+        });
+
+        function updateButton(text) {
+            const sb = document.querySelector(".start");
+            sb.innerText = text;
+            sb.classList.toggle("restart");
+        }
+
+        function startGame() {
+            gameStarted = true;
+            gameSequence = [];
+            userSequence = [];
+            level = 0;
+            h1.textContent = `Simon Game Says`;
+            h2.textContent = `Press any key to start`
+            nextLevel();
+        }
+
+        function nextLevel() {
+            level++;
+            h2.innerText = `Level ${level}`;
+            const randomColor = colors[Math.floor(Math.random() * colors.length)];
+            gameSequence.push(randomColor);
+            playSequence();
+        }
+
+        function playSequence() {
+            let i = 0;
+            const interval = setInterval(function () {
+                const currentColor = gameSequence[i];
+                const button = document.querySelector(`.${currentColor}`);
+                if (button) {
+                    buttonFlash(button);
+                } else {
+                    console.error(`Element with class "${currentColor}" not found.`);
+                }
+        
+                i++;
+                if (i >= gameSequence.length) {
+                    clearInterval(interval);
+                }
+            }, 1000);
+            userSequence = [];
+        }
+        
+
+        function checkUserInput() {
+            if (userSequence.length === gameSequence.length) {
+                for (let i = 0; i < userSequence.length; i++) {
+                    if (userSequence[i] !== gameSequence[i]) {
+                        gameOver();
+                        return;
+                    }
+                }
+                setTimeout(function () {
+                    nextLevel();
+                }, 1000);
             }
         }
 
-        setTimeout(function () {
-            nextLevel();
-        }, 1000);
-    }
-}
+        function gameOver() {
+            gameStarted = false;
+            h1.innerText = `Your Score is ${level - 1}`;
+            h2.innerText = `Game Over! Press any key to restart.`;
 
-function gameOver() {
-    gameStarted = false;
-    h1.innerText=`Your Score is ${level-1}`;
-    h2.innerText = `Game Over! Press any key to restart.`;
-}
+            // Remove the "flash" class from all buttons
+            buttons.forEach((button) => {
+                button.classList.remove("flash");
+            });
+        }
 
-function buttonClick() {
-    if (gameStarted) {
-        const color = this.classList[1];
-        buttonFlash(this);
-        userSequence.push(color);
-        checkUserInput();
-    }
-}
+        function buttonClick() {
+            if (gameStarted) {
+                const color = this.classList[1];
+                buttonFlash(this);
+                userSequence.push(color);
+                checkUserInput();
+            }
+        }
 
-const buttons = document.querySelectorAll(".button");
-buttons.forEach(function (button) {
-    button.addEventListener("click", buttonClick);
-});
-
-
-
+        buttons.forEach(function (button) {
+            button.addEventListener("click", buttonClick);
+        });
